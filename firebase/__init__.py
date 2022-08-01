1a7cac0d86fd705fcdccf97c96527030cbc7b8c1
SeoulSKY <swkim7519@protonmail.ch>
2022-08-01 16:54:28 -0600

    setup firebase

---
+++ firebase/__init__.py
@@ -0,0 +1,21 @@
+import os.path
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+firebase_admin.initialize_app(credentials.Certificate({
+            "type": os.getenv("FIREBASE_TYPE"),
+            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+            "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+            "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+        }))
+
+db = firestore.client()
