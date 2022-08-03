1251584c20259be6573915f54d5337880cfb3dc9
SeoulSKY <swkim7519@protonmail.ch>
2022-08-02 20:52:45 -0600

    Add alert when joining and leaving a Link Play

--- commands/arcaea.py
+++ commands/arcaea.py
@@ -1,12 +1,8 @@
-import asyncio
-
 import discord
 from discord import ui, app_commands, Interaction
 
 import templates
 from commands import Confirm
-from main import ROOT_DIR
-
 
 EMPTY_TEXT = "Empty"
 
@@ -25,9 +21,11 @@ class LinkPlayView(ui.View):
             await interaction.response.send_message("There are no more slots available", ephemeral=True)
             return
 
+        await self._alert_others(interaction.guild, embed, user, f"{user.mention} has joined the Link Play!")
+
         for i in range(0, len(embed.fields)):
             if embed.fields[i].value == EMPTY_TEXT:
-                embed.set_field_at(index=i, name=embed.fields[i].name, value=user.display_name)
+                embed.set_field_at(index=i, name=embed.fields[i].name, value=user.mention)
                 await interaction.message.edit(embed=embed)
 
                 await interaction.response.send_message("Joined!", ephemeral=True)
@@ -35,7 +33,7 @@ class LinkPlayView(ui.View):
 
     def _is_joined(self, embed: discord.Embed, user: discord.User) -> bool:
         for field in embed.fields:
-            if field.value == user.display_name:
+            if field.value == user.mention:
                 return True
 
         return False
@@ -47,6 +45,16 @@ class LinkPlayView(ui.View):
 
         return True
 
+    async def _alert_others(self, guild: discord.Guild, embed: discord.Embed, interacted_user: discord.User,
+                            message: str) -> None:
+
+        for field in embed.fields:
+            if field.value in (EMPTY_TEXT, interacted_user.mention):
+                continue
+
+            user = guild.get_member(int(field.value.removeprefix("<@").removesuffix(">")))
+            await user.send(message)
+
     @ui.button(label="Leave")
     async def leave(self, interaction: Interaction, button: ui.Button):
         embed = interaction.message.embeds[0]
@@ -57,10 +65,11 @@ class LinkPlayView(ui.View):
             return
 
         for i in range(0, len(embed.fields)):
-            if embed.fields[i].value != user.display_name:
+            if embed.fields[i].value != user.mention:
                 continue
 
-            if user.display_name == embed.author.name:
+            lead_user_mention = embed.fields[0].value
+            if user.mention == lead_user_mention:
                 confirm_view = Confirm(confirmed_message="Deleted")
                 await interaction.response.send_message("You're about to delete the Link Play you created. Do you "
                                                         "want to continue?", view=confirm_view, ephemeral=True)
@@ -69,6 +78,8 @@ class LinkPlayView(ui.View):
                 if confirm_view.is_confirmed:
                     await interaction.message.delete()
             else:
+                await self._alert_others(interaction.guild, embed, user, f"{user.mention} has left the Link Play")
+
                 embed.set_field_at(index=i, name=embed.fields[i].name, value=EMPTY_TEXT)
                 await interaction.message.edit(embed=embed)
                 await interaction.response.send_message("You've left the Link Play", ephemeral=True)
@@ -87,21 +98,20 @@ class Arcaea(app_commands.Group):
 
         embed = discord.Embed(color=templates.color,
                               title="Arcaea Link Play",
-                              description=f"**{user.display_name}** is waiting for players to join")
+                              description=f"{user.mention} is waiting for players to join")
 
-        embed.add_field(name="Lead", value=user.display_name)
+        embed.add_field(name="Lead", value=user.mention)
 
         num_players = 3
         for i in range(0, num_players):
             embed.add_field(name="Player", value=EMPTY_TEXT)
 
-        embed.set_author(name=user.display_name, icon_url=user.avatar.url)
+        embed.set_author(name=user.display_name, icon_url=user.display_avatar.url)
         embed.set_footer(text=f"Room code: {roomcode}")
 
-        file = discord.File(f"{ROOT_DIR}/assets/link_play.png", filename="link_play.png")
-        embed.set_thumbnail(url="attachment://link_play.png")
+        embed.set_thumbnail(url="https://user-images.githubusercontent.com/48105703/182501819-502dc5f2-c831-4ce4-8300-78ecc5797b89.png")
 
-        await interaction.response.send_message(file=file, embed=embed, view=LinkPlayView())
+        await interaction.response.send_message(embed=embed, view=LinkPlayView())
         message = await interaction.original_message()
 
         minutes = 30
