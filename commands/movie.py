39134d93a1f81c0a96533ffc070c7c93c331570f
SeoulSKY <email@seoulsky.org>
2024-01-04 00:57:05 -0600

    Implement caching for movies

--- commands/movie.py
+++ commands/movie.py
@@ -1,21 +1,37 @@
 """
 Implements a command relate to movie
 """
+
 import itertools
-from concurrent.futures import ThreadPoolExecutor
-from datetime import timedelta
-from queue import Queue
+import json
+import os
+import threading
 from typing import Literal, Optional
 
 import numpy as np
-from discord import app_commands, Interaction, Embed, NotFound, HTTPException, Message
+from discord import app_commands, Interaction, Embed, HTTPException, Message, NotFound
 from discord.ext import tasks
 from discord.ext.commands import Bot
 from moviepy.editor import VideoFileClip
 from moviepy.video.fx import resize as resizer
-from numpy.core.records import ndarray
+from tqdm import tqdm
 
-from utils import templates
+from utils import templates, constants
+
+DESKTOP_CACHE_PATH = os.path.join(constants.CACHE_DIR, "movie", "desktop")
+"""
+Path to the cache directory for desktop
+"""
+
+MOBILE_CACHE_PATH = os.path.join(constants.CACHE_DIR, "movie", "mobile")
+"""
+Path to the cache directory for mobile
+"""
+
+FPS = 30
+"""
+Frames per second
+"""
 
 CHARS = " ░▒▓█"
 """
@@ -47,56 +63,10 @@ MOBILE_MOVIE_RESOLUTION_16_9 = (11, 6)
 (width, height)
 """
 
-# DO NOT MODIFY
 PIXEL_VALUE_RANGE = 255
 """
 Maximum value of RGB for each pixel
 """
-
-
-class Screen:
-    """
-    Screen to display movie on Discord
-    """
-
-    def __init__(self, message: Message, embed: Embed):
-        self._message = message
-        self._embed = embed
-
-    @property
-    def message(self):
-        """
-        Get the message
-        """
-        return self._message
-
-    @message.setter
-    def message(self, message: Message):
-        self._message = message
-
-    @property
-    def embed(self):
-        """
-        Get the embed
-        """
-        return self._embed
-
-    @embed.setter
-    def embed(self, embed: Embed):
-        self._embed = embed
-
-    async def update(self):
-        """
-        Update the screen on Discord
-        """
-        try:
-            await self.message.edit(embed=self.embed)
-        except HTTPException as ex:
-            if ex.code == 50027:
-                self.message = await self.message.channel.fetch_message(self.message.id)
-                await self.message.edit(embed=self.embed)
-            else:
-                raise ex
 
 
 class Movie(app_commands.Group):
@@ -104,9 +74,50 @@ class Movie(app_commands.Group):
     Commands related to Movie
     """
 
+    _cache: dict[str, list[str]] = {}
+    _num_playing = 0
+    _lock = threading.Lock()
+
     def __init__(self, bot: Bot):
         super().__init__()
         self.bot = bot
+
+        if not os.path.exists(DESKTOP_CACHE_PATH) or not os.path.exists(MOBILE_CACHE_PATH):
+            self._cache_movies()
+
+    @staticmethod
+    def _cache_movies():
+        os.makedirs(constants.CACHE_DIR, exist_ok=True)
+
+        movie_names = [file for file in os.listdir(constants.ASSETS_DIR) if file.endswith(".mp4")]
+        for is_on_mobile in (True, False):
+            os.makedirs(MOBILE_CACHE_PATH if is_on_mobile else DESKTOP_CACHE_PATH, exist_ok=True)
+
+            for movie_name in movie_names:
+                movie = Movie._resize(VideoFileClip(os.path.join(constants.ASSETS_DIR, movie_name),
+                                                    audio=False), is_on_mobile)
+
+                buffer = []
+                with open(Movie._get_cache_path(movie_name.removesuffix(".mp4"), is_on_mobile), "w") as f:
+                    for frame in tqdm(movie.iter_frames(), desc=f"Caching {movie_name} for "
+                                                                f"{'mobile' if is_on_mobile else 'desktop'}",
+                                      total=movie.reader.nframes, unit="frame"):
+                        buffer.append(Movie._create_text(frame, is_on_mobile))
+
+                    f.write(json.dumps(buffer))
+
+    @staticmethod
+    def _get_cache_path(name: str, is_on_mobile: bool) -> str:
+        return os.path.join(MOBILE_CACHE_PATH if is_on_mobile else DESKTOP_CACHE_PATH, name + ".json")
+
+    @staticmethod
+    async def get_frames(name: str, is_on_mobile: bool) -> list[str]:
+        path = Movie._get_cache_path(name, is_on_mobile)
+        if path not in Movie._cache:
+            with open(path, "r") as f:
+                Movie._cache[path] = json.load(f)
+
+        return Movie._cache[path]
 
     @app_commands.command()
     @app_commands.describe(name="Name of the movie to play")
@@ -122,56 +133,56 @@ class Movie(app_commands.Group):
         Play the movie
         """
         is_on_mobile = interaction.guild.get_member(interaction.user.id).is_on_mobile()
-        embed = Embed(color=templates.color, title=name.replace("_", " ").title())
+        embed = Embed(color=templates.color, title=name.replace("_", " ").title(),
+                      description="Loading...")
         await interaction.response.send_message(embed=embed)
 
-        movie = self._resize(VideoFileClip(f"assets/{name}.mp4", audio=False), is_on_mobile)
-
-        text_frames = Queue(maxsize=movie.fps * fps)
-        counter = itertools.count(start=1, step=round(movie.fps) // fps if original_speed else 1)
-        screen = Screen(await interaction.original_response(), embed)
-
-        def _create_frames():
-            try:
-                for i, frame in enumerate(movie.iter_frames()):
-                    # ignore some last frames to prevent reading an empty frame
-                    if i == movie.reader.nframes - 2:
-                        break
-                    if original_speed and i % (movie.fps // fps) != 0:
-                        continue
-
-                    text_frames.put(Movie._create_text(frame, is_on_mobile),
-                                    timeout=timedelta(minutes=3).total_seconds())
-            finally:
-                text_frames.put(None)
+        message: Message = await interaction.original_response()
+        counter = itertools.count(start=0, step=FPS // fps if original_speed else 1)
+        with Movie._lock:
+            Movie._num_playing += 1
+            frames = await Movie.get_frames(name, is_on_mobile)
 
         @tasks.loop(seconds=1 / fps)
-        async def _display():
-            embed.set_footer(text=f"Frame: {next(counter)}")
-            try:
-                text_frame = text_frames.get(timeout=5)
-                if text_frame is None:
-                    text_frames.put(None)
-                    _display.cancel()
-                    return
+        async def display():
+            """
+            Play the movie
+            """
 
-                embed.description = f"```{text_frame}```"
-
-                await screen.update()
-            except NotFound:  # message deleted
-                _display.cancel()
+            index = next(counter)
+            if index >= len(frames):
+                display.cancel()
                 return
 
-        @_display.after_loop
-        async def _clean_up():
-            text_frames.task_done()
-            movie.close()
-            executor.shutdown(wait=False, cancel_futures=True)
+            embed.set_footer(text=f"Frame: {index + 1}")
+            embed.description = f"```{frames[index]}```"
 
-        executor = ThreadPoolExecutor(max_workers=1)
-        executor.submit(_create_frames)
+            nonlocal message
 
-        _display.start()
+            try:
+                await message.edit(embed=embed)
+            except NotFound:  # Message is deleted
+                display.cancel()
+            except HTTPException as ex:
+                if ex.code == 50027:
+                    message = await message.channel.fetch_message(message.id)
+                    await message.edit(embed=embed)
+                else:
+                    raise ex
+
+        @display.after_loop
+        async def clean_up():
+            """
+            Clean up after the movie is finished playing
+            """
+
+            with Movie._lock:
+                Movie._num_playing = max(0, Movie._num_playing - 1)
+
+                if Movie._num_playing <= 0:
+                    Movie._cache.clear()
+
+        display.start()
 
     @staticmethod
     def _resize(video: VideoFileClip, is_on_mobile: bool) -> VideoFileClip:
@@ -189,17 +200,17 @@ class Movie(app_commands.Group):
         return resizer.resize(video, new_resolution)
 
     @staticmethod
-    def _create_text(frame: ndarray, is_on_mobile: bool) -> str:
+    def _create_text(frame: np.ndarray, is_on_mobile: bool) -> str:
         grayscale_frame = np.dot(frame, (0.2989, 0.5870, 0.1140))
-        grayscale_frame = np.floor(grayscale_frame).astype(int)
+        grayscale_frame: np.ndarray = np.floor(grayscale_frame).astype(int)
 
         char_length = len(MOBILE_CHARS) if is_on_mobile else len(CHARS)
-        normalized_frame: ndarray = grayscale_frame * char_length / PIXEL_VALUE_RANGE
+        normalized_frame: np.ndarray = grayscale_frame * char_length / PIXEL_VALUE_RANGE
         normalized_frame = np.floor(normalized_frame).astype(int)
 
         text = ""
-        for arr in normalized_frame:
-            for brightness in arr:
+        for vector in normalized_frame:
+            for brightness in vector:
                 text += (MOBILE_CHARS[brightness] if is_on_mobile else CHARS[brightness]) * 2
             text += "\n"
 
