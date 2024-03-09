b467dddda589cf4d1652ac9486e542de7ec62a12
SeoulSKY <contact@seoulsky.org>
2024-03-09 10:11:01 -0600

    Add example code for character ai

--- sorusora-ai/src/index.ts
+++ sorusora-ai/src/index.ts
@@ -1,12 +1,31 @@
 import dotenv from "dotenv";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const CharacterAI = require("node_characterai");
+
 
 async function main() {
-    dotenv.config();
+  dotenv.config({ path: "../.env" });
 
-    console.log("Hello, World!");
+  console.log("hello world!");
+
+  // const characterAI = new CharacterAI();
+  // await characterAI.authenticateWithToken(process.env.CAI_TOKEN);
+  //
+  // // Place your character's id here
+  // const characterId = "8_1NyR8w1dOXmI1uWaieQcd147hecbdIK7CeEAIrdJw";
+  //
+  // const chat = await characterAI.createOrContinueChat(characterId);
+  //
+  // // Send a message
+  // const response = await chat.sendAndAwaitResponse(
+  //     "Hello discord mod!",
+  //     true
+  // );
+  //
+  // console.log(response);
 }
 
 (async () => {
-    await main();
+  await main();
 })();
